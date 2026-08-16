1691f5902b4536f4215d8f5d5e470142ed88e37d
Nik13599 <KOLYASELEKH@gmail.com>
2026-08-16 20:05:13 +0300

    Refresh iPhone cache for grouped cocktail sorting

--- web-template/sw.js
+++ web-template/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bali-cocktail-v7-alpha';
+const CACHE='bali-cocktail-v8-category-alpha';
 const CORE=['./','./index.html','./cocktails.json','./manifest.webmanifest','./images/ic_launcher.png','./ingredient-zoom.js','./readonly-mobile.js','./sort-alpha.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
