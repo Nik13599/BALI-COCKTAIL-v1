30788437ac1372ffa886c023f2ae334dc2919ae7
Nik13599 <KOLYASELEKH@gmail.com>
2026-08-16 06:43:37 +0300

    Cache alphabetical sorting script in iPhone app

--- web-template/sw.js
+++ web-template/sw.js
@@ -1,5 +1,5 @@
-const CACHE='bali-cocktail-v6-readonly';
-const CORE=['./','./index.html','./cocktails.json','./manifest.webmanifest','./images/ic_launcher.jpg','./ingredient-zoom.js','./readonly-mobile.js'];
+const CACHE='bali-cocktail-v7-alpha';
+const CORE=['./','./index.html','./cocktails.json','./manifest.webmanifest','./images/ic_launcher.png','./ingredient-zoom.js','./readonly-mobile.js','./sort-alpha.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>cached)))});
